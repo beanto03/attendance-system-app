ef9fb1d0fa1315389ff85fc37235cde70014b562
beanto03 <64835144+beanto03@users.noreply.github.com>
2024-06-27 12:15:28 +0800

    Update Home.py
    - change title

--- Home.py
+++ Home.py
@@ -2,11 +2,11 @@ import streamlit as st
 
 
 
-st.set_page_config(page_title='Attendance System',layout='wide')
+st.set_page_config(page_title='Face Recognition System',layout='wide')
 
 st.header('Attendance System using Face Recognition')
 
-with st.spinner("Loading Models and Conneting to Redis db ..."):
+with st.spinner("Loading Models and Connecting to Redis db ..."):
     import face_rec
     
 st.success('Model loaded sucesfully')
